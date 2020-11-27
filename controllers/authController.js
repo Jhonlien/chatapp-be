cc0d0cdda876cd1aa21ea4f154d3731974f5baab
Fandi Adhitya <accountname@Fandis-MacBook-Pro.local>
2020-11-28 03:09:54 +0700

    jwt fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ exports.login = async (req, res) => {
         
         if(!bcrypt.compareSync(password, user.password)) return res.status(404).json({message: "Incorrect Password"})
 
-        const userToken = generateToken(user)
+        const userToken = generateToken(user.get({raw:true}))
         return res.send(userToken)
     }
     catch(e){
@@ -31,8 +31,6 @@ exports.register = async (req, res) => {
 
 const generateToken = (user) => {
     delete user.password
-
-    const token = jwt.sign(user.toJSON(), 'secret', {expiresIn: 86400})
-
-    return {...user, ...{token}}
+    const token = jwt.sign(user, 'secret', {expiresIn: 86400})
+    return {user, token}
 }
